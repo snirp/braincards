1fd3db04d3a2a5facc4cbdc6e80bca98cf44493b
Roy Prins <prinsroy@gmail.com>
2018-12-23 23:19:03 +0100

    Open login and communicate user data

--- project/urls.py
+++ project/urls.py
@@ -5,10 +5,12 @@ from rest.router import router
 from django.conf.urls.static import static
 
 from cards.views import quiz_detail
+from users.views import profile
 from rest import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/profile/', profile),
     path('accounts/', include('allauth.urls')),
     path('rest/quiz/<slug:shortname>/<slug:identifier>', quiz_detail),
     path('rest/', include(router.urls)),
